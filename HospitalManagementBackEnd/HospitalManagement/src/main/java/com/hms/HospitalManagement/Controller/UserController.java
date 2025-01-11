246973b070473edf4840d6aa643b2af1d58461f0
Ajay <87834482+ajaikrishnan@users.noreply.github.com>
2025-01-11 14:48:34 +0530

    Update UserController.java

--- HospitalManagementBackEnd/HospitalManagement/src/main/java/com/hms/HospitalManagement/Controller/UserController.java
+++ HospitalManagementBackEnd/HospitalManagement/src/main/java/com/hms/HospitalManagement/Controller/UserController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-//@CrossOrigin(origins = {"http://localhost:3000", "http://192.168.1.2:3000" , "http://localhost:3001"})
+//@CrossOrigin(origins = {"http://localhost:3000" , "http://localhost:3001"})
 
 @RestController
 @RequestMapping("/users")
